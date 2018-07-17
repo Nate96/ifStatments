bb0b5e9550be6bd755796fa6136caebf9fc27a3e
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-17 16:11:37 -0500

    The program works, need to got back and fix any code smells

--- ifStatements.py
+++ ifStatements.py
@@ -1,7 +1,71 @@
+import random
+
 class ifStatements():
-    def temp(self):
-        print("hello world")
-    
+
+    @staticmethod
+    def _is_even(number):
+        return number % 2 == 0
+
+    @staticmethod
+    def _is_greater_than_ten(number):
+        return number > 10
+
+    @staticmethod
+    def _is_smaller(target, number):
+        return number < target
+
+    @staticmethod
+    def _get_range_hint(symbol):
+        return symbol == 'r' or symbol == 'R'
+
+    @staticmethod
+    def _get_even_or_odd_hint(symbol):
+        return symbol == "e" or symbol == "E"
+
+    @staticmethod
+    def _give_instructions():
+        print()
+
+    @staticmethod
+    def _is_lower(input, target):
+        return input < target
+
+    def _getting_hints(self, hint_symbol, target):
+        if self._get_range_hint(hint_symbol):
+            if self._is_greater_than_ten(target):
+                print("The number is larger than 10")
+            else:
+                print("The number is smaller than 10")
+            return False
+
+        if self._get_even_or_odd_hint(hint_symbol):
+            if self._is_even(target):
+                print("The number is even")
+            else:
+                print("The number is odd")
+            return False
+        return True
+
+    def _guessing_numbers(self, attempt, target):
+        if attempt == target:
+            print("Nailed it!!")
+        elif self._is_lower(attempt, target):
+            print("higher")
+        else:
+            print("lower")
+
+    def run(self):
+        self._give_instructions()
+
+        target = random.randint(1, 21)
+        attempt = raw_input("Guess a number: ")
+
+        while attempt != -1:
+            if self._getting_hints(str(attempt), target):
+                self._guessing_numbers(int(attempt), int(target))
+            attempt = raw_input("Guess a number: ")
+
 
 if __name__ == "__main__":
-    print("hello world")
+    game = ifStatements()
+    game.run()
---
+++ ifStatementsTest.py
@@ -0,0 +1,61 @@
+import unittest
+from ifStatements import ifStatements
+
+
+class TestIfStatement(unittest.TestCase):
+    def setUp(self):
+        self.ifStatements = ifStatements()
+
+    def test_isEvent(self):
+        self.assertTrue(self.ifStatements._is_even(2))
+        self.assertTrue(self.ifStatements._is_even(-4))
+
+        self.assertFalse(self.ifStatements._is_even(5))
+        self.assertFalse(self.ifStatements._is_even(-7))
+
+    def test_is_greater_than_ten(self):
+        self.assertTrue(self.ifStatements._is_greater_than_ten(13))
+
+        self.assertFalse(self.ifStatements._is_greater_than_ten(10))
+
+        self.assertFalse(self.ifStatements._is_greater_than_ten(3))
+
+    def test_is_smaller(self):
+        self.ifStatements.target_number = 5
+
+        self.assertFalse(self.ifStatements._is_smaller(6))
+
+        self.assertTrue(self.ifStatements._is_smaller(4))
+
+    def test_get_range_hint(self):
+        self.assertTrue(self.ifStatements._get_range_hint('r'))
+        self.assertTrue(self.ifStatements._get_range_hint('R'))
+
+        self.assertFalse(self.ifStatements._get_range_hint('h'))
+        self.assertFalse(self.ifStatements._get_range_hint('H'))
+
+    def test_getting_hints(self):
+        self.ifStatements._getting_hints('r', 15)
+        self.ifStatements._getting_hints('r', 5)
+
+        self.ifStatements._getting_hints('e', 15)
+        self.ifStatements._getting_hints('e', 4)
+
+        self.ifStatements._getting_hints(1, 15)
+
+    def test_get_even_or_odd_hint(self):
+        self.assertTrue(self.ifStatements._get_even_or_odd_hint('e'))
+        self.assertTrue(self.ifStatements._get_even_or_odd_hint('E'))
+
+        self.assertFalse(self.ifStatements._get_even_or_odd_hint('h'))
+        self.assertFalse(self.ifStatements._get_even_or_odd_hint('H'))
+
+    def test_guessing_number(self):
+        self.ifStatements._guessing_numbers(5, 6)
+
+        self.ifStatements._guessing_numbers(10, 5)
+
+        self.ifStatements._guessing_numbers(1, 1)
+
+        self.ifStatements._guessing_numbers('r', 3)
+

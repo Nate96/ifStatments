c9509cb47382beb059c0bd1af26cdbd142f3c9ea
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-23 16:55:48 -0500

    Restructured the whole project

--- ifStatements.py
+++ ifStatements.py
@@ -1,71 +1,79 @@
 import random
 
-class ifStatements():
 
-    @staticmethod
-    def _is_even(number):
-        return number % 2 == 0
+def _is_even(number):
+    return number % 2 == 0
 
-    @staticmethod
-    def _is_greater_than_ten(number):
-        return number > 10
 
-    @staticmethod
-    def _is_smaller(target, attempt):
-        return attempt < target
+def _is_greater_than_ten(number):
+    return number > 10
 
-    @staticmethod
-    def _get_range_hint(symbol):
-        return symbol == 'r' or symbol == 'R'
 
-    @staticmethod
-    def _get_even_or_odd_hint(symbol):
-        return symbol == 'o' or symbol == 'O'
+def _is_smaller(target, attempt):
+    return attempt < target
 
-    @staticmethod
-    def _give_instructions():
-        print()
 
-    @staticmethod
-    def _is_lower(attempt, target):
-        return attempt < target
+def _get_range_hint(symbol):
+    return symbol == 'r' or symbol == 'R'
 
-    def _getting_hints(self, hint_symbol, target):
-        if self._get_range_hint(hint_symbol):
-            if self._is_greater_than_ten(target):
-                print("The number is larger than 10")
-            else:
-                print("The number is smaller than 10")
-            return False
 
-        if self._get_even_or_odd_hint(hint_symbol):
-            if self._is_even(target):
-                print("The number is even")
-            else:
-                print("The number is odd")
-            return False
-        return True
+def _get_even_or_odd_hint(symbol):
+    return symbol == 'o' or symbol == 'O'
 
-    def _guessing_numbers(self, attempt, target):
-        if attempt == target:
-            print("Nailed it!!")
-        elif self._is_lower(attempt, target):
-            print("higher")
+
+def _give_instructions():
+    print()
+
+
+def _is_lower(attempt, target):
+    return attempt < target
+
+
+def _getting_hints(hint_symbol, target):
+    if _get_range_hint(hint_symbol):
+        if _is_greater_than_ten(target):
+            print("The number is larger than 10")
         else:
-            print("lower")
+            print("The number is smaller than 10")
+        return False
 
-    def run(self):
-        self._give_instructions()
+    if _get_even_or_odd_hint(hint_symbol):
+        if _is_even(target):
+            print("The number is even")
+        else:
+            print("The number is odd")
+        return False
+    return True
 
-        target = random.randint(1, 21)
+
+def _guessing_numbers(attempt, target):
+    if attempt == target:
+        print("Nailed it!!")
+    elif _is_lower(attempt, target):
+        print("higher")
+    else:
+        print("lower")
+
+
+def run(self):
+    _give_instructions()
+
+    target = random.randint(1, 21)
+    attempt = raw_input("Guess a number: ")
+
+    while attempt != 'e' or attempt != 'E':
+        if self._getting_hints(str(attempt), target):
+            self._guessing_numbers(int(attempt), target)
         attempt = raw_input("Guess a number: ")
-
-        while attempt != 'e' or attempt != 'E':
-            if self._getting_hints(str(attempt), target):
-                self._guessing_numbers(int(attempt), target)
-            attempt = raw_input("Guess a number: ")
 
 
 if __name__ == "__main__":
-    game = ifStatements()
-    game.run()
+    _give_instructions()
+
+    target = random.randint(1, 21)
+    attempt = raw_input("Guess a number: ")
+
+    while attempt != 'e' or attempt != 'E':
+        if _getting_hints(str(attempt), target):
+            _guessing_numbers(int(attempt), target)
+        attempt = raw_input("Guess a number: ")
--- ifStatementsTest.py
+++ ifStatementsTest.py
@@ -1,61 +1,57 @@
 import unittest
-from ifStatements import ifStatements
+import ifStatements
 
 
 class TestIfStatement(unittest.TestCase):
-    def setUp(self):
-        self.ifStatements = ifStatements()
 
     def test_isEvent(self):
-        self.assertTrue(self.ifStatements._is_even(2))
-        self.assertTrue(self.ifStatements._is_even(-4))
+        self.assertTrue(ifStatements._is_even(2))
+        self.assertTrue(ifStatements._is_even(-4))
 
-        self.assertFalse(self.ifStatements._is_even(5))
-        self.assertFalse(self.ifStatements._is_even(-7))
+        self.assertFalse(ifStatements._is_even(5))
+        self.assertFalse(ifStatements._is_even(-7))
 
     def test_is_greater_than_ten(self):
-        self.assertTrue(self.ifStatements._is_greater_than_ten(13))
+        self.assertTrue(ifStatements._is_greater_than_ten(13))
 
-        self.assertFalse(self.ifStatements._is_greater_than_ten(10))
+        self.assertFalse(ifStatements._is_greater_than_ten(10))
 
-        self.assertFalse(self.ifStatements._is_greater_than_ten(3))
+        self.assertFalse(ifStatements._is_greater_than_ten(3))
 
     def test_is_smaller(self):
-        self.ifStatements.target_number = 5
+        self.assertFalse(ifStatements._is_smaller(6, 10))
 
-        self.assertFalse(self.ifStatements._is_smaller(6))
-
-        self.assertTrue(self.ifStatements._is_smaller(4))
+        self.assertTrue(ifStatements._is_smaller(4, 3))
 
     def test_get_range_hint(self):
-        self.assertTrue(self.ifStatements._get_range_hint('r'))
-        self.assertTrue(self.ifStatements._get_range_hint('R'))
+        self.assertTrue(ifStatements._get_range_hint('r'))
+        self.assertTrue(ifStatements._get_range_hint('R'))
 
-        self.assertFalse(self.ifStatements._get_range_hint('h'))
-        self.assertFalse(self.ifStatements._get_range_hint('H'))
+        self.assertFalse(ifStatements._get_range_hint('h'))
+        self.assertFalse(ifStatements._get_range_hint('H'))
 
     def test_getting_hints(self):
-        self.ifStatements._getting_hints('r', 15)
-        self.ifStatements._getting_hints('r', 5)
+        ifStatements._getting_hints('r', 15)
+        ifStatements._getting_hints('R', 5)
 
-        self.ifStatements._getting_hints('e', 15)
-        self.ifStatements._getting_hints('e', 4)
+        ifStatements._getting_hints('o', 15)
+        ifStatements._getting_hints('O', 4)
 
-        self.ifStatements._getting_hints(1, 15)
+        ifStatements._getting_hints(1, 15)
 
     def test_get_even_or_odd_hint(self):
-        self.assertTrue(self.ifStatements._get_even_or_odd_hint('o'))
-        self.assertTrue(self.ifStatements._get_even_or_odd_hint('O'))
+        self.assertTrue(ifStatements._get_even_or_odd_hint('o'))
+        self.assertTrue(ifStatements._get_even_or_odd_hint('O'))
 
-        self.assertFalse(self.ifStatements._get_even_or_odd_hint('h'))
-        self.assertFalse(self.ifStatements._get_even_or_odd_hint('H'))
+        self.assertFalse(ifStatements._get_even_or_odd_hint('h'))
+        self.assertFalse(ifStatements._get_even_or_odd_hint('H'))
 
     def test_guessing_number(self):
-        self.ifStatements._guessing_numbers(5, 6)
+        ifStatements._guessing_numbers(5, 6)
 
-        self.ifStatements._guessing_numbers(10, 5)
+        ifStatements._guessing_numbers(10, 5)
 
-        self.ifStatements._guessing_numbers(1, 1)
+        ifStatements._guessing_numbers(1, 1)
 
-        self.ifStatements._guessing_numbers('r', 3)
+        ifStatements._guessing_numbers('r', 3)
 
